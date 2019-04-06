8cdde4ef9df175efc7cec2d380ae8ffb61690daf
Aniket Pandey <aniketp@iitk.ac.in>
2019-04-06 20:28:54 +0530

    Generalize main script for user input

--- src/main.m
+++ src/main.m
@@ -1,18 +1,18 @@
 % the script file for testing the code
 
 % the interval of approximations
-a = 0;          % the value of the left end-point of the interval
-b = 10;         % the value of the right end-point of the interval
+a = input('a = ');          % the value of the left end-point of the interval
+b = input('b = ');          % the value of the right end-point of the interval
 
 % the function to be approximated
-f = @(x) ((x-a)*(b-x))^11;
+f = @(x) ((x-b)*(a-x))^11;
 
 % approximation grid size
-nGrid = 100;    % size of the grid to be used for approximation
+nGrid = input('nGrid = ');    % size of the grid to be used for approximation
 [xGrid, fGrid] = discreteData(nGrid, a, b, f);
 
 % the number of test evaluation points
-nEval = 10;
+nEval = input('nEval = ');
 % evaluation grid to be used for testing
 xEval = a + (b - a).*rand(1, nEval);
 
